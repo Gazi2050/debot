431dd4113eca6e93ee6b5589b17f939843a3abdf
Gazi2050 <gazi45344@gmail.com>
2025-03-14 23:59:30 +0600

    ui:created initial layout

---
+++ src/components/Form.svelte
@@ -0,0 +1,23 @@
+<script lang="ts">
+</script>
+
+<div class="flex justify-center items-center max-w-[700px] mx-auto h-screen">
+    <div class="w-full">
+        <p class="text-4xl text-center font-bold mb-10">De Bot</p>
+        <div
+            class="flex justify-between items-center bg-zinc-600 px-5 py-1 text-white rounded-t-lg"
+        >
+            <div>
+                <p>German</p>
+            </div>
+            <div class="flex gap-1">
+                <p>copy</p>
+                <p>toggle</p>
+            </div>
+        </div>
+        <textarea
+            class="w-full h-40 resize-none border p-5 rounded-b-lg focus:outline-0"
+            placeholder="type here..."
+        ></textarea>
+    </div>
+</div>
